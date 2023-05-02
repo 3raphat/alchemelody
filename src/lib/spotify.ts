042fd98be26103af7bcea3042aaac057c02b011d
3raphat <treeraphat234@gmail.com>
2023-05-02 14:43:20 +0700

    fix: spotify lib

--- src/lib/spotify.ts
+++ src/lib/spotify.ts
@@ -1,32 +1,10 @@
-const client_id = process.env.SPOTIFY_CLIENT_ID
-const client_secret = process.env.SPOTIFY_CLIENT_SECRET
-const basic = Buffer.from(`${client_id}:${client_secret}`).toString("base64")
-const TOKEN_ENDPOINT = `https://accounts.spotify.com/api/token`
 const BASE_ENDPOINT = "https://api.spotify.com/v1"
 
-const getAccessToken = async (refresh_token: string) => {
-  const response = await fetch(TOKEN_ENDPOINT, {
-    method: "POST",
-    headers: {
-      Authorization: `Basic ${basic}`,
-      "Content-Type": "application/x-www-form-urlencoded",
-    },
-    body: new URLSearchParams({
-      grant_type: "refresh_token",
-      refresh_token,
-    }),
-  })
-
-  return response.json()
-}
-
 export const getTopItems = async (
-  refresh_token: string,
+  access_token: string,
   type: string,
   limit: number
 ) => {
-  const { access_token } = await getAccessToken(refresh_token)
-
   return fetch(
     `${BASE_ENDPOINT}/me/top/${type}?time_range=short_term&limit=${limit}`,
     {
@@ -38,13 +16,11 @@ export const getTopItems = async (
 }
 
 export const getRecommendations = async (
-  refresh_token: string,
+  access_token: string,
   seed_tracks: string,
   limit: number,
   market: string
 ) => {
-  const { access_token } = await getAccessToken(refresh_token)
-
   return fetch(
     `${BASE_ENDPOINT}/recommendations?limit=${limit}&seed_tracks=${seed_tracks}&market=${market}`,
     {
@@ -56,13 +32,11 @@ export const getRecommendations = async (
 }
 
 export const createPlaylist = async (
-  refresh_token: string,
+  access_token: string,
   user_id: string,
   name: string,
   description: string
 ) => {
-  const { access_token } = await getAccessToken(refresh_token)
-
   const res = await fetch(`${BASE_ENDPOINT}/users/${user_id}/playlists`, {
     method: "POST",
     headers: {
@@ -78,12 +52,10 @@ export const createPlaylist = async (
 }
 
 export const addItemsToPlaylist = async (
-  refresh_token: string,
+  access_token: string,
   playlist_id: string,
   uris: string
 ) => {
-  const { access_token } = await getAccessToken(refresh_token)
-
   return fetch(
     `${BASE_ENDPOINT}/playlists/${playlist_id}/tracks?uris=${uris}`,
     {
@@ -96,12 +68,10 @@ export const addItemsToPlaylist = async (
 }
 
 export const addPlaylistCoverImage = async (
-  refresh_token: string,
+  access_token: string,
   playlist_id: string,
   image: string
 ) => {
-  const { access_token } = await getAccessToken(refresh_token)
-
   return fetch(`${BASE_ENDPOINT}/playlists/${playlist_id}/images`, {
     method: "PUT",
     headers: {
